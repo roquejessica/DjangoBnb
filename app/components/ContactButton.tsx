f21db30b5053b2646b778dc5af7099b1a565c88d
Jessica Roque <jessicaroque363@gmail.com>
2025-11-10 04:31:43 +0800

    Landlord Page (static) - Jessica Roque

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,8 @@
+const ContactButton = () => {
+    return (
+        <div className="cursor-pointer py-4 px-6 bg-airbnb text-white rounded-xl mt-6 hover:bg-airbnb-dark transition">
+            Contact
+        </div>
+    )
+}
+export default ContactButton;
