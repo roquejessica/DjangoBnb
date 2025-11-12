3c571e55129d5d35cbe396a8449520185ab61872
Jessica Roque <jessicaroque363@gmail.com>
2025-11-12 14:21:07 +0800

    User menu popup - Jessica Roque - VID2

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -1,7 +1,18 @@
+'use client' //because we would be using click events here
+
+import {useState} from 'react' //state management to make it easy to track when we click something, store it as a temp var
+
+import MenuLink from './MenuLink'
+
 const UserNav = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <div className="p-2 relative inline-block border rounded-full">
-            <button className="flex items-center">
+            <button 
+                onClick={() => setIsOpen(!isOpen)} //it takes the value isOpen, pass it in, if it is true, it will be false, if it is false, it will be true
+                className="flex items-center"
+            >
                 <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="cursor-pointer w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
@@ -9,6 +20,20 @@ const UserNav = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
             </button>
+
+            {isOpen && (  //if isOpen is true, then render the div
+                <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
+                    <MenuLink 
+                        label="Log in"
+                        onClick={() => console.log('Clicked button')}
+                    />
+
+                    <MenuLink 
+                        label="Sign up"
+                        onClick={() => console.log('Clicked button')}
+                    />
+                </div>
+            )}
         </div>
     )
 }
