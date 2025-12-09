abf0b41c9052c530d78f0b808655e82fa1aa4c9e
Jessica Roque <jessicaroque363@gmail.com>
2025-12-09 17:39:19 +0800

    My properties dynamic - Jessica Roque - VID6

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -1,30 +1,29 @@
-'use client' //because we would be using click events here
+'use client';
 
-import {useState} from 'react' //state management to make it easy to track when we click something, store it as a temp var
-
-import MenuLink from './MenuLink'
-import useLoginModal from '@/app/hooks/useLoginModal'
-import useSignupModal from '@/app/hooks/useSignupModal'
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import MenuLink from "./MenuLink";
+import useLoginModal from "../../hooks/useLoginModal";
+import useSignUpModal from "../../hooks/useSignUpModal";
 
 import LogoutButton from "../LogoutButton";
 
 interface UserNavProps {
-    userId?: string | null;
+    userId: string | null;
 }
 
 const UserNav: React.FC<UserNavProps> = ( 
-    { userId } 
+    userId 
 ) => {
+    const router = useRouter();
     const loginModal = useLoginModal();
-    const signupModal = useSignupModal();
-    const [isOpen, setIsOpen] = useState(false)
-
+    const signupModal = useSignUpModal();
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="p-2 relative inline-block border rounded-full">
             <button 
-                onClick={() => setIsOpen(!isOpen)} //it takes the value isOpen, pass it in, if it is true, it will be false, if it is false, it will be true
-                className="flex items-center"
-            >
+                onClick={() => setIsOpen(!isOpen)}
+                className="flex items-center">
                 <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="cursor-pointer w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
@@ -32,11 +31,19 @@ const UserNav: React.FC<UserNavProps> = (
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
             </button>
-
-            {isOpen && ( 
-                <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
+            {isOpen &&(
+                <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col">
                     {userId ? (
-                        <LogoutButton />
+                        <>
+                            <MenuLink
+                                label='My properties'
+                                onClick={() =>{
+                                    setIsOpen(false);
+                                    router.push(`/myproperties`);
+                                }}
+                            />
+                            <LogoutButton />
+                        </>
                     ) : (
                         <>
                             <MenuLink 
