f013953c9d0fe8ad92574890d2fdf46911d740d3
Jessica Roque <jessicaroque363@gmail.com>
2025-12-11 09:05:37 +0800

    My favorites page (with listings) - Jessica Roque - VID7

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -44,6 +44,14 @@ const UserNav: React.FC<UserNavProps> = (
                             />
 
                             <MenuLink
+                                label='My favorites'
+                                onClick={() =>{
+                                    setIsOpen(false);
+                                    router.push(`/myfavorites`);
+                                }}
+                            />
+
+                            <MenuLink
                                 label='My reservations'
                                 onClick={() =>{
                                     setIsOpen(false);
