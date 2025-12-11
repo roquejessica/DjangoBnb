6c7da2574a7be0fc2ff200ad81a5ffe69214c653
Jessica Roque <jessicaroque363@gmail.com>
2025-12-11 08:53:54 +0800

    Make it possible to set property as favorite - Jessica Roque - VID6

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -2,13 +2,14 @@
 
 import { useEffect, useState } from 'react';
 import PropertyListItem from "./PropertyListItem";
-import apiService from '@/app/services/apiService'
+import apiService from '@/app/services/apiService';
 
 export type PropertyType = {
     id: string;
     title: string;
     image_url: string;
     price_per_night: number;
+    is_favorite: boolean;
 }
 
 interface PropertyListProps {
@@ -20,28 +21,56 @@ const PropertyList: React.FC<PropertyListProps> = ({
 }) => {
     const [properties, setProperties] = useState<PropertyType[]>([]);
 
+    const markFavorite = (id: string, is_favorite: boolean) => {
+        const tmpProperties = properties.map((property: PropertyType) => {
+            if (property.id == id) {
+                property.is_favorite = is_favorite
+
+                if (is_favorite) {
+                    console.log('added to list of favorited propreties')
+                } else {
+                    console.log('removed from list')
+                }
+            }
+
+            return property;
+        })
+
+        setProperties(tmpProperties);
+    }
+
     const getProperties = async () => {
         let url = '/api/properties/';
 
-         if (landlord_id){
+        if (landlord_id) {
             url += `?landlord_id=${landlord_id}`
         }
-        const tmpProperties = await apiService.get(url);
-        
-        setProperties(tmpProperties.data);
+
+        const tmpProperties = await apiService.get(url)
+
+        setProperties(tmpProperties.data.map((property: PropertyType) => {
+            if (tmpProperties.favorites.includes(property.id)) {
+                property.is_favorite = true
+            } else {
+                property.is_favorite = false
+            }
+
+            return property
+        }));
     };
+
     useEffect(() => {
-        apiService.get('/api/properties');
-        
         getProperties();
-    }, [landlord_id]);
+    }, []);
+
     return (
         <>
-            {properties.map((property) => {  {/* loops through the properties and returns the objects */}
+            {properties.map((property) => {
                 return (
                     <PropertyListItem 
                         key={property.id}
                         property={property}
+                        markFavorite={(is_favorite: any) => markFavorite(property.id, is_favorite)}
                     />
                 )
             })}
@@ -49,4 +78,4 @@ const PropertyList: React.FC<PropertyListProps> = ({
     )
 }
 
-export default PropertyList
+export default PropertyList;
