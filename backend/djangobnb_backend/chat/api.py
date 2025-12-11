55352a73539bcfb1a393728eebc2c023a8f10123
Jessica Roque <jessicaroque363@gmail.com>
2025-12-11 16:53:51 +0800

    Start new conversation - Jessica Roque - VID7

--- backend/djangobnb_backend/chat/api.py
+++ backend/djangobnb_backend/chat/api.py
@@ -27,3 +27,19 @@ def conversations_detail(request, pk):
             'conversation': conversation_serializer.data,
             'messages': messages_serializer.data
         }, safe=False)
+
+@api_view(['GET'])  #1st step before adding to urls.py
+def conversations_start(request, user_id):
+    conversations = Conversation.objects.filter(users__in=[user_id]).filter(users__in=[request.user.id])
+
+    if conversations.count() > 0:
+        conversation = conversations.first()
+        
+        return JsonResponse({'success': True, 'conversation_id': conversation.id})
+    else:
+        user = User.objects.get(pk=user_id)
+        conversation = Conversation.objects.create()
+        conversation.users.add(request.user)
+        conversation.users.add(user)
+
+        return JsonResponse({'success': True, 'conversation_id': conversation.id})
