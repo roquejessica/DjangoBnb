27dd0d817843601807566107e03117c6e6cdb8a5
Jessica Roque <jessicaroque363@gmail.com>
2025-12-11 08:21:59 +0800

    My reservations dynamic - Jessica Roque - VID6

--- backend/djangobnb_backend/useraccount/api.py
+++ backend/djangobnb_backend/useraccount/api.py
@@ -1,9 +1,9 @@
+from django.core.serializers import serialize
 from .serializers import UserDetailSerializer
 from .models import User
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-
-
+from property.serializers import ReservationsListSerializer
 
 @api_view(['GET'])
 @authentication_classes([])
@@ -13,4 +13,14 @@ def landlord_detail(request, pk):
 
     serializer = UserDetailSerializer(user, many=False)
 
+    return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+def reservations_list(request):
+    reservations = request.user.reservations.all()
+
+    print(reservations)
+    print('user', request.user)
+
+    serializer = ReservationsListSerializer(reservations, many=True)
     return JsonResponse(serializer.data, safe=False)
