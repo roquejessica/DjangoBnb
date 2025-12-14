4ac49588928007e24cdb4075a7c397e8b64edc47
Jessica Roque <jessicaroque363@gmail.com>
2025-12-15 00:20:12 +0800

    Fix the refresh tokens - Jessica Roque - VID8

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,49 @@
 
 import { cookies } from 'next/headers';
 
+export async function handleRefresh() {
+    console.log('handleRefresh');
+
+    const refreshToken = await getRefreshToken();
+
+    const token = await fetch('http://localhost:8000/api/auth/token/refresh/', {
+        method: 'POST',
+        body: JSON.stringify({
+            refresh: refreshToken
+        }),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(async (json) => {
+            console.log('Response - Refresh:', json);
+
+            if (json.access) {
+                const cookieStore = await cookies();
+                cookieStore.set('session_access_token', json.access, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    maxAge: 60 * 60, // 60 minutes
+                    path: '/'
+                });
+
+                return json.access;
+            } else {
+                resetAuthCookies();
+            }
+        })
+        .catch((error) => {
+            console.log('error', error);
+
+            resetAuthCookies();
+        })
+
+    return token;
+}
+
+
 export async function handleLogin(userId: string, accessToken: string, refreshToken: string) {
     try {
         const cookieStore = await cookies();
@@ -51,6 +94,10 @@ export async function getUserId() {
 export async function getAccessToken() {
     let accessToken = (await cookies()).get('session_access_token')?.value;
 
+    if (!accessToken) {
+        accessToken = await handleRefresh();
+    }
+
     return accessToken;
 }
 
@@ -76,7 +123,7 @@ export async function handleJwtRefresh(accessToken: string, refreshToken: string
 
 export async function getConversations() {
     const accessToken = await getAccessToken();
-    
+
     if (!accessToken) {
         return [];
     }
